f9171e7fc85e1334004e472d65fe42cac47c1723
Daniel Golden <dgolden@newrelic.com>
2020-02-26 11:38:37 -0500

    feat: style the maps in the map list

--- nerdlets/geo-ops-nerdlet/MapList.js
+++ nerdlets/geo-ops-nerdlet/MapList.js
@@ -1,9 +1,10 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { Button, Grid, GridItem, Stack, StackItem } from 'nr1';
+import { Button, Grid, GridItem, Stack, StackItem, Icon } from 'nr1';
 
 import { EmptyState, NerdGraphError } from '@newrelic/nr1-community';
 import Toolbar from '../shared/components/Toolbar';
+import GeoMap from './geo-map';
 
 import { deleteMap } from '../shared/services/map';
 import { deleteMapLocationCollection } from '../shared/services/map-location';
@@ -50,8 +51,11 @@ export default class MapList extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      maps: props.maps || []
+      maps: props.maps || [],
+      settingsPopoverActive: false
     };
+
+    this.handleSettingsPopover = this.handleSettingsPopover.bind(this);
   }
 
   componentDidUpdate(prevProps) {
@@ -70,6 +74,74 @@ export default class MapList extends PureComponent {
     this.props.onMapDelete({ map });
   }
 
+  handleSettingsPopover(e) {
+    this.setState(prevState => ({
+      settingsPopoverActive: !prevState.settingsPopoverActive
+    }));
+    e.stopPropagation();
+  }
+
+  renderSettingsButton(map, navigation) {
+    return (
+      <div
+        className={`service-settings-button-container ${
+          this.state.settingsPopoverActive
+            ? 'settings-popover-active'
+            : 'settings-popover-inactive'
+        }`}
+      >
+        <Button
+          sizeType={Button.SIZE_TYPE.MEDIUM}
+          className="service-settings-button"
+          type={Button.TYPE.NORMAL}
+          iconType={Button.ICON_TYPE.INTERFACE__OPERATIONS__CONFIGURE}
+          onClick={this.handleSettingsPopover}
+        />
+        <ul className="service-settings-dropdown">
+          <li
+            className="service-settings-dropdown-item"
+            onClick={() => navigation.editWizard({ map, activeStep: 2 })}
+          >
+            <Icon type={Icon.TYPE.INTERFACE__INFO__INFO} />
+            Guided Configuration
+          </li>
+          <li
+            className="service-settings-dropdown-item"
+            onClick={() => navigation.edit({ guid: map.guid })}
+          >
+            <Icon type={Icon.TYPE.INTERFACE__OPERATIONS__EDIT} />
+            Edit Map
+          </li>
+          <li
+            className="service-settings-dropdown-item destructive"
+            onClick={() => this.deleteMap({ map })}
+          >
+            <Icon
+              type={Icon.TYPE.INTERFACE__OPERATIONS__TRASH}
+              color="#BF0016"
+            />
+            Delete Map
+          </li>
+          <li
+            className="service-settings-dropdown-item destructive"
+            onClick={async () =>
+              deleteMapLocationCollection({
+                accountId: map.accountId,
+                mapGuid: map.guid
+              })
+            }
+          >
+            <Icon
+              type={Icon.TYPE.INTERFACE__OPERATIONS__TRASH}
+              color="#BF0016"
+            />
+            Delete Markers
+          </li>
+        </ul>
+      </div>
+    );
+  }
+
   render() {
     const { navigation } = this.props;
     const { maps } = this.state;
@@ -86,43 +158,38 @@ export default class MapList extends PureComponent {
       console.log(url);
 
       return (
-        <GridItem columnSpan={4} key={map.guid}>
-          <EmptyState
-            heading={map.title || map.guid}
-            buttonText="Edit Map"
-            buttonOnClick={() => navigation.edit({ guid: map.guid })}
-          />
-          <Stack horizontalType={Stack.HORIZONTAL_TYPE.FILL_EVENLY}>
-            <StackItem>
-              <Button onClick={() => this.deleteMap({ map })}>
-                Delete Map
-              </Button>
-            </StackItem>
-            <StackItem>
-              <Button
-                onClick={() => navigation.editWizard({ map, activeStep: 2 })}
-              >
-                Guided Configuration
-              </Button>
-            </StackItem>
-            <StackItem>
-              <Button onClick={() => navigation.viewMap({ map })}>
-                View Map
-              </Button>
-            </StackItem>
-            <StackItem>
-              <Button
-                onClick={async () =>
-                  deleteMapLocationCollection({
-                    accountId: map.accountId,
-                    mapGuid: map.guid
-                  })
-                }
-              >
-                Delete Markers
-              </Button>
-            </StackItem>
-          </Stack>
+        <GridItem columnSpan={3} key={map.guid}>
+          <div className="map-grid-item">
+            <section className="map-grid-item-preview">
+              <GeoMap
+                accountId={map.accountId}
+                map={map}
+                center={[map.lat, map.lng]}
+                zoom={map.zoom}
+              />
+              <div className="map-grid-item-preview-hover-contents">
+                <Button
+                  onClick={() => this.props.navigation.edit({ guid: map.guid })}
+                  className="map-grid-item-preview-hover-contents-button"
+                >
+                  View map
+                </Button>
+              </div>
+            </section>
+            <Stack
+              fullWidth
+              verticalType={Stack.VERTICAL_TYPE.CENTER}
+              className="map-grid-item-bottom"
+            >
+              <StackItem className="map-grid-item-meta">
+                <h4 className="map-grid-item-name">{map.title || map.guid}</h4>
+                <h6 className="map-grid-item-subtitle">24 locations</h6>
+              </StackItem>
+              <StackItem className="map-grid-item-settings-button">
+                {this.renderSettingsButton(map, navigation)}
+              </StackItem>
+            </Stack>
+          </div>
         </GridItem>
       );
     });
@@ -135,17 +202,13 @@ export default class MapList extends PureComponent {
         />
 
         <Grid
-          className="primary-grid"
+          className="primary-grid map-list-primary-grid"
           spacingType={[Grid.SPACING_TYPE.NONE, Grid.SPACING_TYPE.NONE]}
         >
-          <GridItem
-            columnSpan={12}
-            fullHeight
-            className="locations-table-grid-item"
-          >
+          <GridItem columnSpan={12} fullHeight>
             <Grid>
               {mapGridItems}
-              <GridItem columnSpan={4}>
+              <GridItem columnSpan={3}>
                 <EmptyState
                   heading="+"
                   buttonText="Create New Map"
