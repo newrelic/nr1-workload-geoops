newrelic/nr1-workload-geoops

commit 13c7b8284a211b6addc6fc7835d265c653a494dc

fix(routing): when navigating to create a map, reset selectedMap state

nerdlets/geo-ops-nerdlet/index.js
@@ -119,7 +119,16 @@ export default class index extends PureComponent {
   router({ to, state }) {
     const next = { ...initialPages };
     next[to] = true;
-    this.setState({ pages: next, ...state });
+
+    const newState = {
+      ...state
+    };
+
+    if (to === 'createMap') {
+      newState.selectedMap = null;
+    }
+
+    this.setState({ pages: next, ...newState });
   }
 
   /*

commit 49a0f7bfea7917a9cc5ba1bcd720ab2c6582407f

fix: regression to routing from 13c7b82

nerdlets/geo-ops-nerdlet/index.js
@@ -123,7 +123,9 @@ export default class index extends PureComponent {
     };
 
     if (to === 'createMap') {
-      newState.selectedMap = null;
+      if (!newState.selectedMap) {
+        newState.selectedMap = null;
+      }
     }
 
     this.setState({ pages: next, ...newState });