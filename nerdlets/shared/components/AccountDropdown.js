d00fead6dea5cf123496477ce90fe335efd5ce65
DevFreddy <michael.frederickjr@gmail.com>
2020-06-09 17:48:20 -0400

    fix: update edit map account so it reflects in the ui
    - passed down `formData` prop (Json Schema Form)
    - after loading accounts, use map data to set the current account
    - when map changes (and then formData changes) update currently selected account

--- nerdlets/shared/components/AccountDropdown.js
+++ nerdlets/shared/components/AccountDropdown.js
@@ -6,7 +6,8 @@ export default class AccountDropdown extends React.Component {
   static propTypes = {
     className: PropTypes.string,
     onChange: PropTypes.func,
-    style: PropTypes.object
+    style: PropTypes.object,
+    formData: PropTypes.number
   };
 
   constructor(props) {
@@ -31,6 +32,10 @@ export default class AccountDropdown extends React.Component {
     if (account && (!prevAccount || account.id !== prevAccount.id)) {
       this.props.onChange(account.id);
     }
+
+    if (this.props.formData !== prevProps.formData) {
+      this.setSelectedAccountFromProps();
+    }
   }
 
   async loadAccounts() {
@@ -38,12 +43,38 @@ export default class AccountDropdown extends React.Component {
   }
 
   handleLoadAccountsResponse({ accounts }) {
-    const defaultAccount = accounts[0];
+    const { formData: accountId } = this.props;
+    let selectedAccount = accounts[0];
+
+    if (accountId) {
+      const account = accounts.find(i => i.id === accountId);
+      if (account) {
+        selectedAccount = account;
+      }
+    }
 
     this.setState({
       accounts,
-      selected: defaultAccount
+      selected: selectedAccount
     });
+  }
+
+  setSelectedAccountFromProps() {
+    const { formData: accountId } = this.props;
+    const { accounts } = this.state;
+
+    if (accountId && accounts) {
+      const account = accounts.find(i => i.id === accountId);
+
+      if (!account) {
+        // TO DO
+        // Set a default that warns about "This map is associated to an unknown account"
+      }
+
+      if (account) {
+        this.setState({ selected: account });
+      }
+    }
   }
 
   async loadAccountsWithAccountsQuery() {
