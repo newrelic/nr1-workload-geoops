42afd64d3624a2deb52d7ef5f8d9beb88f8bc5ed
Joel Worrall <jworrall@newrelic.com>
2020-08-20 11:01:52 -0400

    chore: fix jest tests

--- nerdlets/geo-ops-nerdlet/ViewMap/tests/Timeline.test.js
+++ nerdlets/geo-ops-nerdlet/ViewMap/tests/Timeline.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import '../../../setupTests';
 import Timeline from '../Timeline';
 import { locationExample } from './mocks';
-import '../../../setupTests';
 
 const wrapper = shallow(<Timeline />);
 
