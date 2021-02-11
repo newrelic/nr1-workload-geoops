0393bbf7081df03f9e7a5c245507392bbeb06e59
Bob Jackson <bjackson@newrelic.com>
2021-02-11 07:30:51 -0500

    processed EntitySummary.js through prettier and eslint

--- nerdlets/geo-ops-nerdlet/ViewMap/EntitySummary.js
+++ nerdlets/geo-ops-nerdlet/ViewMap/EntitySummary.js
@@ -1,27 +1,28 @@
 import set from 'lodash.set';
 
 const composeEntitySummary = (activeLocationEntities = []) => {
-	if (activeLocationEntities.length > 0) {
-		return activeLocationEntities.map(entity => {
-			console.debug("If no alertSeverity, set to NOT_CONFIGURED");
-			if (entity.alertSeverity == null) set(entity, 'alertSeverity', 'NOT_CONFIGURED');
-			return {
-				name: entity.name,
-				alertSeverity: entity.alertSeverity,
-				type: entity.type,
-				guid: entity.guid,
-				reporting: entity.reporting
-			};
-		});
-	} else {
-		return [
-			{
-				name: 'hi',
-				alertSeverity: 'NOT_ALERTING',
-				type: 'APPLICATION'
-			}
-		];
-	}
+  if (activeLocationEntities.length > 0) {
+    return activeLocationEntities.map(entity => {
+      //      console.debug('If no alertSeverity, set to NOT_CONFIGURED');
+      if (entity.alertSeverity == null)
+        set(entity, 'alertSeverity', 'NOT_CONFIGURED');
+      return {
+        name: entity.name,
+        alertSeverity: entity.alertSeverity,
+        type: entity.type,
+        guid: entity.guid,
+        reporting: entity.reporting
+      };
+    });
+  } else {
+    return [
+      {
+        name: 'hi',
+        alertSeverity: 'NOT_ALERTING',
+        type: 'APPLICATION'
+      }
+    ];
+  }
 };
 
 export default composeEntitySummary;
